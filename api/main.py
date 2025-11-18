b4ccb332c8efe170ef5724007bf678d579c02cb8
Ama Aljuffry <amaaljuffry@gmail.com>
2025-11-18 09:53:18 +0800

    add new domain

--- api/main.py
+++ api/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "https://petai-frontend.netlify.app",
+        "https://petai.ama24.my/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://*.vercel.app",
+        "https://petai.ama24.my/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
